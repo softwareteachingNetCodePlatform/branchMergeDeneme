5bd9d4199db8690491d913f869fd1bfb44ce17ae
softwareteachingNetCodePlatform <111507000+softwareteachingNetCodePlatform@users.noreply.github.com>
2022-08-21 13:41:49 +0300

    Update branchC01.java

--- src/branchC01.java
+++ src/branchC01.java
@@ -5,5 +5,7 @@ public class branchC01 {
         //3.branch onaylamak için lokalden giden
         //4.branch hasan yapıldı ve lokalden yola çıktı :-)
         System.out.println("Yeni deneme1");
+        System.out.println("Yeni deneme2");
+        
     }
 }
