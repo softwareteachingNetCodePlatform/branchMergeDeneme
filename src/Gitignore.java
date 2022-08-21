fc7700849b735eca6567b9e2a9acc2d22efb05bd
hasan <getdatapro@gmail.com>
2022-08-21 16:33:37 +0300

    V2

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -1,6 +1,7 @@
 public class Gitignore {
     public static void main(String[] args) {
         System.out.println("GitIgnore Deneme-1");
+        System.out.println("GitIgnore Deneme-2");
 
     }
 }
